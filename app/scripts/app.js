890eb7573683287ddead1c32848235ae90d0c9ad
Taras Grabets <TarasGrabets@Livenation.com>
2016-03-25 18:29:22 +0200

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name psjwtApp
+ * @description
+ * # psjwtApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('psjwtApp', ['ui.router']);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name psjwtApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the psjwtApp
+ */
+angular.module('psjwtApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
