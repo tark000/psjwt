fabab102103e7227f1f43bc9f59b23ea50f44287
Taras Grabets <TarasGrabets@Livenation.com>
2016-03-25 19:22:20 +0200

    reg

---
+++ app/scripts/directives/validateEquals.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name psjwtApp.directive:sameAs
+ * @description
+ * # sameAs
+ */
+angular.module('psjwtApp')
+  .directive('validateEquals', function () {
+    return {
+      require: 'ngModel',
+      link: function(scope, element,atrs,ngModelCtrl) {
+     	function valideEqual(value) {
+     		var valid = (value === scope.$eval(attrs.validateEquals));
+     		ngModelCtrl.$setValidity('equal', valid);
+     		return valid ? value : undefined;
+     	}
+     	ngModelCtrl.$parsers.push(valideEqual);
+     	ngModelCtrl.$formatters.push(valideEqual);
+
+     	scope.$watch(attrs.validateEquals, function(){
+     		ngModelCtrl.$setViewValue(ngModelCtrl.$viewValue);
+     	})
+
+
+      }
+    };
+  });
